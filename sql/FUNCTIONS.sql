76330e0d7a5f8ea8be42b66b4fa71f18878f2403
Danii Rylski <addanik@gmail.com>
2020-12-10 23:06:55 +0300

    Sync scripts with server

--- sql/FUNCTIONS.sql
+++ sql/FUNCTIONS.sql
@@ -11,3 +11,17 @@ BEGIN
 
     RETURN user_type;
 END;
+
+CREATE OR REPLACE FUNCTION GET_ID(
+    LOGIN IN VARCHAR2,
+    PASS IN VARCHAR2
+)
+    RETURN NUMBER
+    IS
+    r_user_id NUMBER(6,0);
+BEGIN
+    SELECT usr.user_id INTO r_user_id  FROM "USER" usr
+    WHERE usr.login = login and usr.password = pass;
+
+    RETURN r_user_id;
+END;
