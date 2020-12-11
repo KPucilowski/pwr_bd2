811683bca0be27b997c6d9c81c7fe51a0ae661cc
Danii Rylski <addanik@gmail.com>
2020-12-12 00:24:04 +0300

    Fix login scripts

--- sql/FUNCTIONS.sql
+++ sql/FUNCTIONS.sql
@@ -1,27 +1,27 @@
 CREATE OR REPLACE FUNCTION CHECK_LOGIN(
-    LOGIN IN VARCHAR2,
-    PASS IN VARCHAR2
-) 
-RETURN VARCHAR2
-IS
+    LOGIN_INP IN VARCHAR2,
+    PASS_INP IN VARCHAR2
+)
+    RETURN VARCHAR2
+    IS
     user_type VARCHAR2(255);
 BEGIN
-    SELECT usr."TYPE" INTO user_type  FROM "USER" usr
-    WHERE usr.login = login and usr.password = pass;
+    SELECT usr."TYPE" INTO user_type FROM "USER" usr
+    WHERE usr.login = LOGIN_INP and usr.password = PASS_INP;
 
     RETURN user_type;
 END;
 
 CREATE OR REPLACE FUNCTION GET_ID(
-    LOGIN IN VARCHAR2,
-    PASS IN VARCHAR2
+    LOGIN_INP IN VARCHAR2,
+    PASS_INP IN VARCHAR2
 )
     RETURN NUMBER
     IS
     r_user_id NUMBER(6,0);
 BEGIN
-    SELECT usr.user_id INTO r_user_id  FROM "USER" usr
-    WHERE usr.login = login and usr.password = pass;
+    SELECT usr.user_id INTO r_user_id FROM "USER" usr
+    WHERE usr.login = LOGIN_INP and usr.password = PASS_INP;
 
     RETURN r_user_id;
 END;
