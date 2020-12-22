e7978cef83e85db2face2bba629a3f06c2fc653f
Danii Rylski <addanik@gmail.com>
2020-12-22 21:24:59 +0300

    Professor model

--- src/bd2/models/ProfessorModel.java
+++ src/bd2/models/ProfessorModel.java
@@ -1,4 +1,18 @@
 package bd2.models;
 
-public class ProfessorModel {
+import bd2.App;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ProfessorModel extends UserModel {
+    public ProfessorModel(int id, String type) {
+        super(id, type);
+    }
+
+    public ResultSet getGroupStudents(int group_id) throws SQLException {
+        Statement st = App.cn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        return st.executeQuery("select * from BD.GROUP_STUDENT_VIEW where professor_id = " + this.id + " group_id = " + group_id);
+    }
 }
