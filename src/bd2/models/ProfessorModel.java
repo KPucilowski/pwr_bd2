0a7cf7c18d459d0f36e219974c59caaac40bbad7
Danii Rylski <addanik@gmail.com>
2020-12-22 21:54:09 +0300

    Adding/removing grades by professor

--- src/bd2/models/ProfessorModel.java
+++ src/bd2/models/ProfessorModel.java
@@ -2,13 +2,34 @@ package bd2.models;
 
 import bd2.App;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class ProfessorModel extends UserModel {
+    private ResultSet groups;
+
     public ProfessorModel(int id, String type) {
         super(id, type);
+    }
+
+    private void removeGrade(int student_id, int group_id) throws SQLException {
+        CallableStatement stmt = App.cn.prepareCall("{call BD.REMOVE_GRADE(?, ?)}");
+        stmt.setInt(1, student_id);
+        stmt.setInt(2, group_id);
+        stmt.execute();
+    }
+
+    /* Can also use to edit grade */
+    private void addGrade(int student_id, int group_id, double grade) throws SQLException {
+        CallableStatement stmt = App.cn.prepareCall("{call BD.ADD_GRADE(?, ?, ?)}");
+        stmt.setInt(1, student_id);
+        stmt.setInt(2, group_id);
+        stmt.setDouble(3, grade);
+        stmt.execute();
+    }
+
+    private void fetchGroups() throws SQLException {
+        Statement st = App.cn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        groups = st.executeQuery("select * from BD.STUDENT_GRADES_VIEW where student_id = " + this.id);
     }
 
     public ResultSet getGroupStudents(int group_id) throws SQLException {
