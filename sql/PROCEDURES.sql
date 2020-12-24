869a7b8d82b4b51be1b2fb96ca4b1717c96e077b
Danii Rylski <addanik@gmail.com>
2020-12-24 22:05:49 +0300

    Add group sql + reformat

--- sql/PROCEDURES.sql
+++ sql/PROCEDURES.sql
@@ -1,7 +1,4 @@
 --------------------------------------------------------
---  File created - âòîðíèê-äåêàáðÿ-01-2020   
---------------------------------------------------------
---------------------------------------------------------
 --  DDL for Procedure ADD_GRADE
 --------------------------------------------------------
 
@@ -30,89 +27,27 @@ BEGIN
   END IF;
 END;
 --------------------------------------------------------
---  DDL for Procedure ADD_STUDENT_TO_GROUP
+--  DDL for Procedure ADD_GROUP
 --------------------------------------------------------
 
-CREATE OR REPLACE NONEDITIONABLE PROCEDURE "ADD_STUDENT_TO_GROUP" (
-    student_id_in NUMBER,
-    group_in NUMBER
-    )
-IS
-    student_count NUMBER;
-    student_limit NUMBER;
-BEGIN
-    SELECT count(*) INTO student_count FROM "RECORD" rec
-    WHERE
-        rec.group_id = group_in;
-
-    SELECT grp.student_limit INTO student_limit FROM "GROUP" grp
-    WHERE
-        grp.group_id = group_in;
-
-    IF student_count < student_limit THEN
-        INSERT INTO "RECORD"(student_id, group_id, record_date)
-        VALUES (student_id_in, group_in, CURRENT_DATE);
-    END IF;
-END;
---------------------------------------------------------
---  DDL for Procedure CLOSE_GROUP
---------------------------------------------------------
-
-CREATE OR REPLACE NONEDITIONABLE PROCEDURE "CLOSE_GROUP" (group_in NUMBER)
-IS
-BEGIN
-    DELETE FROM "RECORD" rec
-    WHERE 
-        rec.group_id = group_in;
-
-    DELETE FROM "GROUP" grp
-    WHERE
-        grp.group_id = group_in;
-END;
---------------------------------------------------------
---  DDL for Procedure REMOVE_GRADE
---------------------------------------------------------
-
-CREATE OR REPLACE NONEDITIONABLE PROCEDURE "REMOVE_GRADE" (
-    student_id NUMBER,
-    group_id_n NUMBER)
-IS
-    grade_var NUMBER;
-BEGIN
-    SELECT COUNT(*) INTO grade_var FROM "RECORD" rec
-    WHERE 
-        student_id = rec.student_id and
-        group_id_n = rec.group_id;
-
-    IF grade_var = 1 THEN
-        UPDATE "RECORD" rec
-        SET 
-            GRADE = NULL,
-            GRADE_DATE = NULL
-        WHERE 
-            student_id = rec.student_id and
-            group_id_n = rec.group_id;
-        dbms_output.put_line('Student grade has been removed.');
-  ELSE
-        dbms_output.put_line('Student record does not exist.');
-  END IF;
-END;
---------------------------------------------------------
---  DDL for Procedure REMOVE_STUDENT_FROM_GROUP
---------------------------------------------------------
-
-CREATE OR REPLACE NONEDITIONABLE PROCEDURE "REMOVE_STUDENT_FROM_GROUP"
+CREATE OR REPLACE NONEDITIONABLE PROCEDURE "ADD_GROUP"
 (
-  STUDENT_IN IN NUMBER 
-, GROUP_IN IN NUMBER 
-) AS 
+    GROUP_ID_IN IN NUMBER
+    , SUBJECT_ID_IN IN NUMBER
+    , PROFESSOR_ID_IN IN NUMBER
+    , PARITY_IN IN VARCHAR2
+    , DAY_IN IN NUMBER
+    , TIME_STR_IN IN VARCHAR2
+    , FORM_IN IN VARCHAR2
+    , STUDENT_LIMIT IN NUMBER
+) AS
+    TIME_IN TIMESTAMP;
 BEGIN
-  DELETE FROM "RECORD" rec
-  WHERE
-    group_in = rec.group_id and
-    student_in = rec.student_id;
-    
-END REMOVE_STUDENT_FROM_GROUP;
+    TIME_IN := TO_TIMESTAMP(TIME_STR_IN, 'HH24:MI');
+
+    insert into "GROUP" ("GROUP_ID", SUBJECT_ID, PROFESSOR_ID, "PARITY", "DAY", "TIME", "FORM", STUDENT_LIMIT)
+    values (GROUP_ID_IN, SUBJECT_ID_IN, PROFESSOR_ID_IN, PARITY_IN, DAY_IN, TIME_IN, FORM_IN, STUDENT_LIMIT);
+END ADD_GROUP;
 --------------------------------------------------------
 --  DDL for Procedure ADD_STUDENT
 --------------------------------------------------------
@@ -141,6 +76,74 @@ BEGIN
     values (TEMP_ID, PESEL_IN, SYSDATE, 1, 1, null);
 END ADD_STUDENT;
 --------------------------------------------------------
+--  DDL for Procedure ADD_STUDENT_TO_GROUP
+--------------------------------------------------------
+
+CREATE OR REPLACE NONEDITIONABLE PROCEDURE "ADD_STUDENT_TO_GROUP" (
+    student_id_in NUMBER,
+    group_in NUMBER
+    )
+IS
+    student_count NUMBER;
+    student_limit NUMBER;
+BEGIN
+    SELECT count(*) INTO student_count FROM "RECORD" rec
+    WHERE
+        rec.group_id = group_in;
+
+    SELECT grp.student_limit INTO student_limit FROM "GROUP" grp
+    WHERE
+        grp.group_id = group_in;
+
+    IF student_count < student_limit THEN
+        INSERT INTO "RECORD"(student_id, group_id, record_date)
+        VALUES (student_id_in, group_in, CURRENT_DATE);
+    END IF;
+END;
+--------------------------------------------------------
+--  DDL for Procedure REMOVE_GRADE
+--------------------------------------------------------
+
+CREATE OR REPLACE NONEDITIONABLE PROCEDURE "REMOVE_GRADE" (
+    student_id NUMBER,
+    group_id_n NUMBER)
+    IS
+    grade_var NUMBER;
+BEGIN
+    SELECT COUNT(*) INTO grade_var FROM "RECORD" rec
+    WHERE
+            student_id = rec.student_id and
+            group_id_n = rec.group_id;
+
+    IF grade_var = 1 THEN
+        UPDATE "RECORD" rec
+        SET
+            GRADE = NULL,
+            GRADE_DATE = NULL
+        WHERE
+                student_id = rec.student_id and
+                group_id_n = rec.group_id;
+        dbms_output.put_line('Student grade has been removed.');
+    ELSE
+        dbms_output.put_line('Student record does not exist.');
+    END IF;
+END;
+--------------------------------------------------------
+--  DDL for Procedure REMOVE_GROUP
+--------------------------------------------------------
+
+CREATE OR REPLACE NONEDITIONABLE PROCEDURE "REMOVE_GROUP" (group_in NUMBER)
+IS
+BEGIN
+    DELETE FROM "RECORD" rec
+    WHERE 
+        rec.group_id = group_in;
+
+    DELETE FROM "GROUP" grp
+    WHERE
+        grp.group_id = group_in;
+END;
+--------------------------------------------------------
 --  DDL for Procedure REMOVE_STUDENT
 --------------------------------------------------------
 
@@ -165,3 +168,19 @@ BEGIN
     WHERE
             STUDENT_ID_IN = pd.user_id;
 END REMOVE_STUDENT;
+--------------------------------------------------------
+--  DDL for Procedure REMOVE_STUDENT_FROM_GROUP
+--------------------------------------------------------
+
+CREATE OR REPLACE NONEDITIONABLE PROCEDURE "REMOVE_STUDENT_FROM_GROUP"
+(
+  STUDENT_IN IN NUMBER 
+, GROUP_IN IN NUMBER 
+) AS 
+BEGIN
+  DELETE FROM "RECORD" rec
+  WHERE
+    group_in = rec.group_id and
+    student_in = rec.student_id;
+
+END REMOVE_STUDENT_FROM_GROUP;
