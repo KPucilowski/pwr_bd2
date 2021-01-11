6a09b3caf7206114fad9dd7978319bf750551b75
dmatviichuk <matviichuk.dmytro@gmail.com>
2021-01-11 18:49:39 +0100

    New student view

---
+++ src/bd2/views/NewStudentView.java
@@ -0,0 +1,58 @@
+package bd2.views;
+
+import javax.swing.*;
+
+public class NewStudentView {
+    private JFrame frame;
+    private JPanel newStudentPanel;
+    public JButton okButton;
+    public JButton cancelButton;
+    public JTextField txtFirstName;
+    public JTextField txtLastName;
+    public JTextField txtFacultyID;
+    public JTextField txtPESEL;
+
+    public NewStudentView( ){
+        frame = new JFrame("Student menu");
+        frame.setContentPane(newStudentPanel);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        getCancelButton().addActionListener(e->dispose());
+    }
+
+    public void dispose() {
+        frame.dispose(); ;
+    }
+
+    public JButton getOkButton() {
+        return okButton;
+    }
+
+    public JButton getCancelButton() {
+        return cancelButton;
+    }
+
+    public JTextField getTxtFirstName() {
+        return txtFirstName;
+    }
+
+    public JTextField getTxtLastName() {
+        return txtLastName;
+    }
+
+    public JTextField getTxtFacultyID() {
+        return txtFacultyID;
+    }
+
+    public JTextField getTxtPESEL() {
+        return txtPESEL;
+    }
+
+
+
+
+
+
+}
